c0d3b4caddc68ba78b693a253c76122ffd026de2
wayson <wayson1225@outlook.com>
2022-10-08 12:47:47 -0700

    Mongoose Queries and Passport Callbacks

--- services/passport.js
+++ services/passport.js
@@ -13,6 +13,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // We already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // We don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
       new User({ googleId: profile.id }).save();
     }
   )
