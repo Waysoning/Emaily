b70e042e0dbd482cd1b4718fe32753def803f404
wayson <wayson1225@outlook.com>
2022-10-08 23:46:36 -0700

    connect fecthUser

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => {
@@ -14,7 +16,7 @@ const Landing = () => {
 
 class App extends Component {
   componentDidMount() {
-    // axios.get('/api/current_user');
+    this.props.fetchUser();
   }
 
   render() {
@@ -33,4 +35,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
