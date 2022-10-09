7b79cf3e9d39d81ba244bc7dbc9b27e2dee64d6d
wayson <wayson1225@outlook.com>
2022-10-08 22:48:58 -0700

    React Router Setup

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => {
+  return <h2>Header</h2>;
+};
+const Dashboard = () => {
+  return <h2>Dashboard</h2>;
+};
+const SurveyNew = () => {
+  return <h2>SurveyNew</h2>;
+};
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
 
 const App = () => {
   return <h1>Hello, world!!!</h1>;
