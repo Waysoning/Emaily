72b61bc859a7c5fc78099099149072ce856d5c02
wayson <wayson1225@outlook.com>
2022-10-10 03:26:42 -0700

    Feedback for User Feedback

--- config/prod.js
+++ config/prod.js
@@ -7,4 +7,5 @@ module.exports = {
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
   senderEmail: process.env.SENDER_EMAIL,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
 };
