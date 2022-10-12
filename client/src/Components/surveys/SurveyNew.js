b0cfb97f3aded862777a9dd5a278ea86df49511a
wayson <wayson1225@outlook.com>
2022-10-11 21:42:03 -0700

    clear the data when back

--- client/src/Components/surveys/SurveyNew.js
+++ client/src/Components/surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -25,4 +26,7 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+// reset the date on the form when the user clicks the back button
+export default reduxForm({
+  form: 'surveyForm',
+})(SurveyNew);
